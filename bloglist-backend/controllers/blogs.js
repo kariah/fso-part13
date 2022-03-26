dbcb603c925b9173e7180568261028c53849a178
Kari Heiskanen <kari.a.heiskanen@gmail.com>
2022-03-26 09:02:10 +0200

    Tehtävä 13.10

--- bloglist-backend/controllers/blogs.js
+++ bloglist-backend/controllers/blogs.js
@@ -1,6 +1,24 @@
 const router = require('express').Router() 
 const { Blog, User } = require('../models')
 require('express-async-errors');
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('../utils/config')
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      console.log(authorization.substring(7))
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch (error){
+      console.log(error)
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
 
 const blogFinder = async (req, res, next) => {
     req.blog = await Blog.findByPk(req.params.id)
@@ -33,12 +51,14 @@ router.get('/:id', blogFinder, async (req, res, next) => {
     }
 })
 
-router.post('/', async (req, res, next) => {
-    console.log('post ', req.body)
-    // const user = await User.findOne()
-    // const note = await Blpg.create({ ...req.body, userId: user.id })
-    try {
-        const blog = await Blog.create(req.body)
+router.post('/', tokenExtractor, async (req, res, next) => {
+    console.log('post ', req.body) 
+    try { 
+        const user = await User.findByPk(req.decodedToken.id) 
+        const blog = await Blog.create({...req.body, userId: user.id, date: new Date()})
+   
+        //console.log('blog ', blog)
+
         return res.json(blog)
     } catch (error) {
         // return res.status(400).json({ error })
--- bloglist-backend/controllers/login.js
+++ bloglist-backend/controllers/login.js
@@ -1,33 +1,71 @@
-const { User, Blog } = require('../models') 
+// const { User, Blog } = require('../models') 
+
+// const jwt = require('jsonwebtoken')
+// const { SECRET } = require('../utils/config')
+// const router = require('express').Router()
+
+// const tokenExtractor = (req, res, next) => {
+//     const authorization = req.get('authorization')
+//     if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+//         try {
+//             console.log(authorization.substring(7))
+//             req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+//         } catch (error) {
+//             console.log(error)
+//             return res.status(401).json({ error: 'token invalid' })
+//         }
+//     } else {
+//         return res.status(401).json({ error: 'token missing' })
+//     }
+//     next()
+// }
+
+// router.post('/', tokenExtractor, async (req, res) => {
+//     try {
+//         const user = await User.findByPk(req.decodedToken.id)
+//         const blog = await Blog.create({ ...req.body, userId: user.id, date: new Date() })
+//         res.json(blog)
+//     } catch (error) {
+//         return res.status(400).json({ error })
+//     }
+// })
+
+// module.exports = router
+
 
 const jwt = require('jsonwebtoken')
-const { SECRET } = require('../utils/config')
 const router = require('express').Router()
 
-const tokenExtractor = (req, res, next) => {
-    const authorization = req.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        try {
-            console.log(authorization.substring(7))
-            req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
-        } catch (error) {
-            console.log(error)
-            return res.status(401).json({ error: 'token invalid' })
-        }
-    } else {
-        return res.status(401).json({ error: 'token missing' })
-    }
-    next()
-}
+const { SECRET } = require('../utils/config')
+const User = require('../models/user')
 
-router.post('/', tokenExtractor, async (req, res) => {
-    try {
-        const user = await User.findByPk(req.decodedToken.id)
-        const note = await Blog.create({ ...req.body, userId: user.id, date: new Date() })
-        res.json(note)
-    } catch (error) {
-        return res.status(400).json({ error })
+router.post('/', async (request, response) => {
+  const body = request.body
+
+  const user = await User.findOne({
+    where: {
+      username: body.username
     }
+  })
+
+  const passwordCorrect = body.password === 'salainen'
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user.id,
+  }
+
+  const token = jwt.sign(userForToken, SECRET)
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
 })
 
 module.exports = router
