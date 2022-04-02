bb714c173ba734eb70dc4e83adf3ff1105e194fb
Kari Heiskanen <kari.a.heiskanen@gmail.com>
2022-04-02 11:43:30 +0300

    Tehtävä 13.19

--- bloglist-backend/migrations/20220401_01_create_year_to_blogs.js
+++ bloglist-backend/migrations/20220401_01_create_year_to_blogs.js
@@ -1,5 +1,4 @@
 const { DataTypes } = require('sequelize')
-const { sequelize } = require('../utils/db')
 
 module.exports = {
   up: async ({ context: queryInterface }) => {
--- bloglist-backend/models/index.js
+++ bloglist-backend/models/index.js
@@ -1,10 +1,10 @@
 const Blog = require('./blog')
 const User = require('./user')
+const Readinglist = require('./readinglist')
 
 User.hasMany(Blog)
-Blog.belongsTo(User) 
+Blog.belongsTo(User)  
 
-// User.sync({ alter: true })
-// Blog.sync({ alter: true })
+User.belongsToMany(Blog, { through: Readinglist }) 
 
-module.exports = { Blog, User }
+module.exports = { Blog, User, Readinglist }
